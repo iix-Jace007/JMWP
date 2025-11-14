b222c91ac4ff5a599f53a63b5f4cc445dc6bbbe6
Jace <juliaarinsberg07@gmail.com>
2025-11-14 12:41:53 -0500

    finished Item class, Added Abadon and Abu sticks, added textures

--- src/main/java/net/Jace007/JMWP/item/ModItems.java
+++ src/main/java/net/Jace007/JMWP/item/ModItems.java
@@ -1,4 +1,21 @@
 package net.Jace007.JMWP.item;
 
+import net.Jace007.JMWP.Jmwp;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Jmwp.MOD_ID);
+
+    // Insert Items Below
+    public static final DeferredItem<Item> ABADON_STICK = ITEMS.register("abadon_stick",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> ABU_STICK =  ITEMS.register("abu_stick",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
